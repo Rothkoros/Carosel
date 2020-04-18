3930868a79a85012e2183141824ba20924dfca94
Devin Curtis <devincurtis@UA105350927.localdomain>
2020-04-18 16:48:50 -0600

    Fixed 'er up

--- bookInfo.js
+++ bookInfo.js
@@ -1,36 +1,37 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 function bookinfo(interest) {
-    const text = interest.Activity + "<br>" + interest.desc;
-    document.getElementById("information").innerHTML = text;
-    // var data_file = "products.json";
-    // var http_request = new XMLHttpRequest();
+  console.log(interest);
+  debugger;
+  const text = interest.Activity + "<br>" + interest.desc;
+  document.getElementById("information").innerHTML = text;
+  // var data_file = "products.json";
+  // var http_request = new XMLHttpRequest();
 
-    // http_request.onreadystatechange = function () {
-    //     if (http_request.readyState == 4) {
-    //         // Javascript function JSON.parse to parse JSON data
-    //         var jsonObj = JSON.parse(http_request.responseText);
+  // http_request.onreadystatechange = function () {
+  //     if (http_request.readyState == 4) {
+  //         // Javascript function JSON.parse to parse JSON data
+  //         var jsonObj = JSON.parse(http_request.responseText);
 
-    //         // jsonObj variable now contains the data structure and can
-    //         // be accessed as jsonObj.name and jsonObj.country.
-    //         var text = "";
-    //         for (var i = 0; i < jsonObj.book.length; i++) {
-    //             if (jsonObj.book[i].id == t) {
-    //                 text = text + jsonObj.book[i].Title + " " + jsonObj.book[i].Author + "<br>"+jsonObj.book[i].desc;
-    //                 var selection = i;
-    //             }
+  //         // jsonObj variable now contains the data structure and can
+  //         // be accessed as jsonObj.name and jsonObj.country.
+  //         var text = "";
+  //         for (var i = 0; i < jsonObj.book.length; i++) {
+  //             if (jsonObj.book[i].id == t) {
+  //                 text = text + jsonObj.book[i].Title + " " + jsonObj.book[i].Author + "<br>"+jsonObj.book[i].desc;
+  //                 var selection = i;
+  //             }
 
-    //         }
-    //         console.log(selection);
-    //         document.getElementById("information").innerHTML = text;
+  //         }
+  //         console.log(selection);
+  //         document.getElementById("information").innerHTML = text;
 
-    //     }
-    // }
-    // http_request.open("GET", data_file, true);
-    // http_request.send();
+  //     }
+  // }
+  // http_request.open("GET", data_file, true);
+  // http_request.send();
 }
-
---
+++ logic.js
@@ -0,0 +1,32 @@
+let html = "";
+$(document).ready(function () {
+  console.log("Document ready");
+  $.getJSON("products.json", function (data) {
+    console.log("Received data");
+    $.each(data, function (key, value) {
+      $.each(value, function (index, interest) {
+        console.log(index + " " + interest.Activity);
+        html += '<div class= "books">';
+        html += "<h2>" + interest.Activity + "</h2>";
+        html += "<h4>" + interest.desc + "</h4>";
+        html += `<img src="${interest.Source}" style="width:100px;height:100px"></div>`;
+      });
+    });
+    $("#books").html(html);
+    console.log(html);
+    $("#books").cycle({
+      fx: "fade",
+      pause: "1",
+      prev: "#prev",
+      next: "#next",
+      speed: 500,
+      timeout: 1000,
+    });
+  });
+});
+
+function bookinfo(interest) {
+  console.log(interest);
+  const text = interest.Activity + "<br>" + interest.desc;
+  document.getElementById("information").innerHTML = text;
+}
